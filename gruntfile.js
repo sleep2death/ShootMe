1da8f8037ed467e67568c01524478f22234987fe
Aspirin <aspirin2d@outlook.com>
2015-05-15 13:41:13 +0800

    change the name to lower case

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,61 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        src: './src/WonderCraft.ts',
-        target: 'es5',
-        dest: {
-            dev: "./build/wondercraft-dev.js",
-        },
-        refs: {
-            ref: './src/_references.ts',
-            phaser: './typings/phaser/phaser.d.ts',
-            browserify: './typings/browserify/browserify.d.ts'
-        },
-        ts: {
-            dev: {
-                src: ["<%= refs.browserify %>", "<%= refs.ref %>", "<%= refs.phaser %>", "<%= src %>"],
-                out: "<%= dest.dev %>",
-            },
-            options: {
-                verbose: true,
-                module: 'commonjs',
-                target: '<%= target %>',
-                sourceMap: true,
-                declaration: false,
-                fast: 'never'
-            }
-        },
-        clean: {
-            dev: ["build/*.*"]
-        },
-        watch: {
-            dev: {
-                files: ['src/**/*.ts'],
-                tasks: ['clean:dev', 'ts:dev', 'browserify:dev'],
-                options: {},
-            },
-        },
-        browserify: {
-            dev: {
-                files: {
-                    "<%= dest.dev %>": [" <%= dest.dev %> "]
-                }
-            },
+module.exports = function ( grunt ) {
+	grunt.initConfig( {
+		src: './src/WonderCraft.ts',
+		target: 'es5',
+		dest: {
+			dev: "./build/wondercraft-dev.js",
+		},
+		refs: {
+			ref: './src/_references.ts',
+			phaser: './typings/phaser/phaser.d.ts',
+			browserify: './typings/browserify/browserify.d.ts'
+		},
+		ts: {
+			dev: {
+				src: [ "<%= refs.browserify %>", "<%= refs.ref %>", "<%= refs.phaser %>", "<%= src %>" ],
+				out: "<%= dest.dev %>",
+			},
+			options: {
+				verbose: true,
+				module: 'commonjs',
+				target: '<%= target %>',
+				sourceMap: true,
+				declaration: false,
+				fast: 'never'
+			}
+		},
+		clean: {
+			dev: [ "build/*.*" ]
+		},
+		watch: {
+			dev: {
+				files: [ 'src/**/*.ts' ],
+				tasks: [ 'clean:dev', 'ts:dev', 'browserify:dev', "copy:main" ],
+				options: {},
+			},
+		},
+		browserify: {
+			dev: {
+				files: {
+					"<%= dest.dev %>": [ " <%= dest.dev %> " ]
+				}
+			},
 
-            options: {}
-        },
-    });
+			options: {}
+		},
+		copy: {
+			main: {
+				src: 'node_modules/phaser/build/phaser.min.js',
+				dest: 'build/phaser.min.js',
+			},
+		},
+	} );
 
-    grunt.loadNpmTasks('grunt-ts');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks( 'grunt-ts' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-browserify' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 
-    grunt.registerTask("default", ["watch:dev"]);
+	grunt.registerTask( "default", [ "watch:dev" ] );
 };
